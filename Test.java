756fd71597fa01f20e69805e6712dc0934a7b9aa
Rohit Kulkarni <97961145+Rohitk-2686@users.noreply.github.com>
2025-11-13 11:51:01 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test
+  {
+    public static void main(String[] args)
+    {
+      int n=10
+      System.out.println("Squares of numbers 1-10:");
+      for(i=1;i<=10;i++)
+        {
+          int res=i*i;
+          System.out.println("Square of "+i+" = "+res);
+        }
+    }
+  }
